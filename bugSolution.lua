471ed6287cf43d6deab981b975a9a68554fe5fd1
BugHunter-X <askzenai@gmail.com>
2025-02-14 05:26:05 +0100

    Add bugSolution.lua file

---
+++ bugSolution.lua
@@ -0,0 +1,9 @@
+local function foo(x)
+  if x == nil then return 0 end -- Handle nil input
+  return x + 1
+end
+
+print(foo(nil)) -- Output: 0
+print(foo(5)) -- Output: 6
+print(foo()) -- Output: 0
+print(foo(nil)) -- Output: 0
